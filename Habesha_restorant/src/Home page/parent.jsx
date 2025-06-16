3f6180e580df20062dfc84d2ee228d4484b039fd
Aschalew Daraje <aschalewdere2@gmail.com>
2025-06-16 10:26:59 -0700

    lucid react library installed for the icon usage

--- Habesha_restorant/src/Home page/parent.jsx
+++ Habesha_restorant/src/Home page/parent.jsx
@@ -1,10 +1,12 @@
 import Hero from "./hero_section"
 import GalleryPage from "./gallery"
+import CustomerReviews from "./customer_reviews";
 export default function Parent(){
     return(
         <>
         <Hero/>
         <GalleryPage/>
+        <CustomerReviews/>
         </>
     );
 }
